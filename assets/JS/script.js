f928ca059a5e923254c78aa51f0603f230d6d2b5
blanebussey <blanebussey@gmail.com>
2021-10-31 18:18:29 -0500

    initial html and java

---
+++ assets/JS/script.js
@@ -0,0 +1,38 @@
+var apiKey = "7a6b3354e50774f952a848fe125c2899"
+var nameInputEl = document.querySelector("#city");
+var userFormEl = document.querySelector("#weathercast")
+var currentCity = document.getElementById("cityName")
+
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var cityWeather = nameInputEl.value.trim();
+
+    if(cityWeather) {
+        getCity(cityWeather) 
+        nameInputEl.value = "";   
+    }
+}
+var getCity = function(cityName) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+
+    fetch(apiUrl).then(function(response){ 
+       return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        currentCity.textContent = data.name
+
+    })
+}
+
+//var displayCities = function(name of the city variable){
+// localstorage.getItem
+// add the city to the results
+// localstorage.setItem 
+// run a for loop to create the buttons   
+//}  
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
